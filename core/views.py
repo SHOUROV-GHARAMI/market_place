16a4570cd10944ed50c0dc424349d9ebe4e3de08
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-09-07 18:02:50 +0600

    new update

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from item.models import Category, Item
+
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:10]
+    categories = Category.objects.all()
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'item': items,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
